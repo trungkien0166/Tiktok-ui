529c41108c586fa390ce3103d7bc14d67077566c
trungkien0166 <nguyenkien.skd@gmail.com>
2024-03-06 22:07:42 +0700

    Cài đặt babel-plugin-module-resolver

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Content from './Content';
-
+import Button from '~/components/Button';
 function App() {
   return (
     <div className="App">
+      <Button />
       <Content />   mình là Trung Kiên
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Content() {
     return (
         <>
-            xin chào các bạn
+            xin chào các bạn nhé
         </>
     );
 }
